601e1e1b1a300bc6f65fe1b6950ff2db3550b5c2
Godwin Aniefiok Stephen <67881043+df6ty4bit@users.noreply.github.com>
2025-08-03 10:13:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     // ===== BOT CORE SETTINGS =====
-    SESSION_ID: process.env.SESSION_ID || "IK~tP1UEYAB#KPbgURt7mK44wfIrnEq_tIk_fRWp89nbS30BJM0H9WM",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "IK~JaFklawT#NrLchyIkDI7-YjXfz3Z7IKGLvACvLkE1qBhhF55wxbQ",  // Your bot's session ID (keep it secure)
     PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
     CHATBOT: getConfig("CHATBOT") || "on", // on/off chat bot 
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "Stephen Assistant",  // Bot's display name
